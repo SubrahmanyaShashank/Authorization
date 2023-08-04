17452a516f77fe093ea49754a9bbee76f6fee78f
SubrahmanyaShashank <140293161+SubrahmanyaShashank@users.noreply.github.com>
2023-08-04 12:29:02 +0530

    Add files via upload

---
+++ src/main/java/com/example/LoginPassword/LoginPasswordApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginPassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPasswordApplication.class, args);
+	}
+
+}
